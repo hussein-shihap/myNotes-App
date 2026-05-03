3bd8dcd62904d72995cf0684f9ace8b4316d982c
Hadi <hadi@example.com>
2026-05-03 14:01:43 +0300

    project backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/firebase_options.dart';
 import 'screens/login_screen.dart';
-
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart'; 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); 
+    await Firebase.initializeApp( 
+     options: DefaultFirebaseOptions.currentPlatform, 
+);
   runApp(MyApp());
 }
 
--- lib/screens/edit_note_screen.dart
+++ lib/screens/edit_note_screen.dart
@@ -1,13 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+
+
+Future<void> updateNote(String docId, String newTitle, String newInfo) async {
+  await FirebaseFirestore.instance
+      .collection('Notes')
+      .doc(docId)
+      .update({
+        'info': newInfo,
+        'title': newTitle,
+      });
+}
+
+
+
+
+
+
+
+
+
 
 class EditNoteScreen extends StatefulWidget {
   final String title;
   final String content;
+  final String docId;
 
   const EditNoteScreen({
     super.key,
     required this.title,
-    required this.content,
+    required this.content, required this.docId,
   });
 
   @override
@@ -77,8 +100,8 @@ class _EditNoteScreenState extends State<EditNoteScreen> {
                   backgroundColor: Colors.blue,
                 ),
 
-                onPressed: () {
-
+                onPressed: () async {
+                  await updateNote(widget.docId, titleController.text, contentController.text);
                   Navigator.pop(context, {
                     "title": titleController.text,
                     "content": contentController.text,
